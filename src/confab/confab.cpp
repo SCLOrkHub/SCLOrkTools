250c3152a41cb617ef17d70c403ef54a40cbf951
Luke Nihlen <luke.nihlen@gmail.com>
2019-04-25 21:05:58 -0700

    adds gflags and a quick test

--- src/confab/confab.cpp
+++ src/confab/confab.cpp
@@ -1,5 +1,11 @@
+#include <gflags/gflags.h>
+
 #include <iostream>
 
+DEFINE_string(database_file_path, "../../data/confab.db", "Path to the LevelDB database file to use for content");
+
 int main(int argc, char* argv[]) {
-    std::cout << "hello, world!" << std::endl;
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+
 }
