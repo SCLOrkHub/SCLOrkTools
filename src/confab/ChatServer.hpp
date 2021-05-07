3d96eca5eea3bcf7e74c3541e8f8ad5cf6d6efaa
Luke Nihlen <luke.nihlen@gmail.com>
2021-05-07 12:49:14 -0700

    Adds additional logging, supports logging to file (#37)
    * adds message response throttling
    
    * adds support for optional file logging

--- src/confab/ChatServer.hpp
+++ src/confab/ChatServer.hpp
@@ -14,7 +14,7 @@ namespace Confab {
  */
 class ChatServer {
 public:
-    ChatServer(int32_t timeout);
+    ChatServer(int32_t timeout, int32_t maxMessagesPerRequest);
     ~ChatServer();
 
     bool create(const std::string& bindPort);
@@ -38,7 +38,7 @@ private:
     lo_server_thread m_tcpThread;
     lo_server m_tcpServer;
 
-    std::chrono::system_clock::time_point m_lastUpdateTime;
+    std::chrono::steady_clock::time_point m_lastUpdateTime;
 
     int m_userSerial;
 
@@ -47,9 +47,11 @@ private:
 
     // Map of userID to most recent ping time, used to check for timeouts.
     std::chrono::seconds m_timeout;
-    std::unordered_map<int, std::chrono::system_clock::time_point> m_clientPings;
+    std::unordered_map<int, std::chrono::steady_clock::time_point> m_clientPings;
 
-    static const int kMessageArraySize = 1024;
+    int m_maxMessagesPerRequest;
+
+    static const int kMessageArraySize = 128;
     int m_messageSerial;
     std::array<const char*, kMessageArraySize> m_paths;
     std::array<lo_message, kMessageArraySize> m_messages;
--- src/confab/confab-server.cpp
+++ src/confab/confab-server.cpp
@@ -4,9 +4,11 @@
 
 #include "fmt/core.h"
 #include "gflags/gflags.h"
-#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
+#include <memory>
 #include <pthread.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -15,13 +17,24 @@
 // Command line flags for the HTTP server.
 DEFINE_int32(chatPort, 61010, "OSC TCP port for incoming chat messgaes");
 DEFINE_int32(timeout, 10, "The timeout in seconds before automatically disconnecting an unresponsive client.");
+DEFINE_int32(maxMessagesPerRequest, 3, "Maximum number of messages to respond to per update request.");
+DEFINE_string(logFile, "", "A path to log to a file to. If not provided, file logging is disabled.");
 
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    auto logger = spdlog::stdout_color_mt("console");
-    spdlog::set_default_logger(logger);
-    spdlog::info("Starting confab-server v{} on pid {}", Confab::confabVersion.toString(), getpid());
+    auto consoleLog = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    consoleLog->set_level(spdlog::level::info);
+
+    std::shared_ptr<spdlog::sinks::basic_file_sink_mt> fileLog;
+    if (FLAGS_logFile.size() > 0) {
+        spdlog::info("additionally logging to file at {}", FLAGS_logFile);
+        fileLog = std::make_shared<spdlog::sinks::basic_file_sink_mt>(FLAGS_logFile.data(), false);
+        fileLog->set_level(spdlog::level::info);
+        spdlog::default_logger()->sinks().push_back(fileLog);
+    }
+
+    spdlog::info("====== starting confab-server v{} on pid {}", Confab::confabVersion.toString(), getpid());
 
     // Create thread masks for ignoring SIGINT signals here, as the OSC handler will catch the SIGINT and terminate the
     // program itself on that.
@@ -35,7 +48,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    Confab::ChatServer chatServer(FLAGS_timeout);
+    Confab::ChatServer chatServer(FLAGS_timeout, FLAGS_maxMessagesPerRequest);
     if (!chatServer.create(fmt::format("{}", FLAGS_chatPort))) {
         spdlog::error("Failed to create chat server on port {}", FLAGS_chatPort);
         return -1;
